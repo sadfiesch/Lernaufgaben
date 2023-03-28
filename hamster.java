a933d61ae3d0722054bd61f5f1b07856d9614d23
pedel13 <wetpat13@mail.com>
2023-03-28 20:55:01 +0200

    bitte kontroliere

---
+++ hamster.java
@@ -0,0 +1,21 @@
+public class hamster {
+    public static void main (String[]args){
+
+        String hamster = ("");/*nur das de cood kei fehlärmäldig git (wer de signalgeber) */
+        boolean fornFrei = true;/*nur das de cood kei fehlärmäldig git (wer de sensorempfänger) */
+        int linienLaufen = 0;
+
+        while (linienLaufen < 4) {
+            fornFrei = true;
+            feldForn = true;
+            if (fornFrei == true){
+                hamster = ("vor");
+            }
+            if (fornFrei == false){
+                hamster = ("kertwendung"); 
+            }
+            linienLaufen++;
+        }
+    }
+    
+}
