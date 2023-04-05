839188d31fff4fa23949bc880b16717d5565facd
pedel13 <wetpat13@mail.com>
2023-04-05 23:28:51 +0200

    erweiterung mit linienmesser

--- hansterSpirale.java
+++ hansterSpirale.java
@@ -1,9 +1,19 @@
 import de.hamster.debugger.model.Territorium;import de.hamster.debugger.model.Territory;import de.hamster.model.HamsterException;import de.hamster.model.HamsterInitialisierungsException;import de.hamster.model.HamsterNichtInitialisiertException;import de.hamster.model.KachelLeerException;import de.hamster.model.MauerDaException;import de.hamster.model.MaulLeerException;import de.hamster.model.MouthEmptyException;import de.hamster.model.WallInFrontException;import de.hamster.model.TileEmptyException;public class hansterSpirale extends de.hamster.debugger.model.IHamster implements de.hamster.model.HamsterProgram {public void main() {
-
+	
+	int messen = 1;
+	
+	while (vornFrei()){
+		gib();
+		vor();
+		messen++;
+	}
+	(messen - 1);
+	linksUm();
 	int spirale = 1;
-	while (spirale < 10){
+		
+	while (spirale < messen){
 		int linien = spirale;
-		while (linien < 10){
+		while (linien < messen){
 			gib();
 			vor();
 			linien++;
