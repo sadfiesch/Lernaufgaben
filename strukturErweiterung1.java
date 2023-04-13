a12109cb7ca59ff18a39cf77628f98dbb366dd1c
pedel13 <wetpat13@mail.com>
2023-04-13 19:41:43 +0200

    leuft im Hamsterprogramm

---
+++ strukturErweiterung1.java
@@ -0,0 +1,19 @@
+import de.hamster.debugger.model.Territorium;import de.hamster.debugger.model.Territory;import de.hamster.model.HamsterException;import de.hamster.model.HamsterInitialisierungsException;import de.hamster.model.HamsterNichtInitialisiertException;import de.hamster.model.KachelLeerException;import de.hamster.model.MauerDaException;import de.hamster.model.MaulLeerException;import de.hamster.model.MouthEmptyException;import de.hamster.model.WallInFrontException;import de.hamster.model.TileEmptyException;public class strukturErweiterung1 extends de.hamster.debugger.model.IHamster implements de.hamster.model.HamsterProgram {public void main() {
+
+	int korn = 0;
+	while (vornFrei()){
+		if (kornDa()){
+			linksUm();
+			nimm();
+			korn++;
+		}
+		else {
+			vor();
+		}
+	}    
+	linksUm();
+	for (int i = 0;i < korn;i++){
+		vor();
+	}
+}
+}
