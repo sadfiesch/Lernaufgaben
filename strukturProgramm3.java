a2c83008cc5ddb0c509e402706adc3c9c93eef1a
pedel13 <wetpat13@mail.com>
2023-04-14 22:03:22 +0200

    erweiterung

--- strukturProgramm3.java
+++ strukturProgramm3.java
@@ -1,8 +1,10 @@
 import de.hamster.debugger.model.Territorium;import de.hamster.debugger.model.Territory;import de.hamster.model.HamsterException;import de.hamster.model.HamsterInitialisierungsException;import de.hamster.model.HamsterNichtInitialisiertException;import de.hamster.model.KachelLeerException;import de.hamster.model.MauerDaException;import de.hamster.model.MaulLeerException;import de.hamster.model.MouthEmptyException;import de.hamster.model.WallInFrontException;import de.hamster.model.TileEmptyException;public class strukturProgramm3 extends de.hamster.debugger.model.IHamster implements de.hamster.model.HamsterProgram {public void main() {
 
+	int zehler = 0;
 	while (vornFrei() || !kornDa()){
 		if (kornDa()){
 			nimm();
+			zehler++;
 		}
 		if (vornFrei()){
 			vor();
@@ -16,6 +18,25 @@ import de.hamster.debugger.model.Territorium;import de.hamster.debugger.model.Te
 			linksUm();
 			vor();
 			linksUm();
+		}
+	}
+	linksUm();
+	if (zehler == 4){
+		vor();	
+	}
+	if (zehler == 2 || zehler == 3){
+		for (int i = 0;i < 2;i++){
+			vor();
+		}
+	}
+	if (zehler == 1){
+		for (int i = 0;i < 3;i++){
+			vor();
+		}
+	}
+	if (zehler > 4){
+		for (int i = 0;i < 4;i++){
+			vor();
 		}
 	}
 }
--- strukturprogramm2.java
+++ strukturprogramm2.java
@@ -1,11 +1,17 @@
 import de.hamster.debugger.model.Territorium;import de.hamster.debugger.model.Territory;import de.hamster.model.HamsterException;import de.hamster.model.HamsterInitialisierungsException;import de.hamster.model.HamsterNichtInitialisiertException;import de.hamster.model.KachelLeerException;import de.hamster.model.MauerDaException;import de.hamster.model.MaulLeerException;import de.hamster.model.MouthEmptyException;import de.hamster.model.WallInFrontException;import de.hamster.model.TileEmptyException;public class strukturprogramm2 extends de.hamster.debugger.model.IHamster implements de.hamster.model.HamsterProgram {public void main() {
     
+    int wegZehler = 0;
+    int arrayPosition = 0;
+    int[] strecke = new int[3];
     while (vornFrei()){
     	if (kornDa()){
     		linksUm();
     		linksUm();
     		linksUm();
     		nimm();
+    		strecke[arrayPosition] = wegZehler;
+    		arrayPosition++;
+    		wegZehler = 0;
     		if (kornDa()){
     			nimm();
     			break;
@@ -13,7 +19,24 @@ import de.hamster.debugger.model.Territorium;import de.hamster.debugger.model.Te
     	}
     	else  {
     		vor();
+    		wegZehler++;
     	}
     }
+    linksUm();
+    linksUm();
+    linksUm();
+    for (int i = 0;i < strecke[2];i++){
+    	vor();
+    }
+    linksUm();
+    for (int i = 0;i < strecke[1];i++){
+    	vor();
+    }
+    linksUm();
+    for (int i = 0;i < strecke[0];i++){
+    	vor();
+    }
+    linksUm();
+    linksUm();
 }
 }
