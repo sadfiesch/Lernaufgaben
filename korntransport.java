d93a1553caee6dc70fdba03e49717be95e228017
pedel13 <wetpat13@mail.com>
2023-04-15 10:49:15 +0200

    mit methoden

---
+++ korntransport.java
@@ -0,0 +1,28 @@
+import de.hamster.debugger.model.Territorium;import de.hamster.debugger.model.Territory;import de.hamster.model.HamsterException;import de.hamster.model.HamsterInitialisierungsException;import de.hamster.model.HamsterNichtInitialisiertException;import de.hamster.model.KachelLeerException;import de.hamster.model.MauerDaException;import de.hamster.model.MaulLeerException;import de.hamster.model.MouthEmptyException;import de.hamster.model.WallInFrontException;import de.hamster.model.TileEmptyException;public class korntransport extends de.hamster.debugger.model.IHamster implements de.hamster.model.HamsterProgram {public void main() {
+
+	korn();
+}
+void korn(){
+
+   	if (maulLeer() && kornDa()){
+    	nimm();
+   	}
+   	else{
+   		if (!maulLeer()) {
+   			gib();
+   		}
+   	}
+   	if (kornDa() || !maulLeer()){
+   		laufZurWand();
+   	}    
+}
+void laufZurWand(){
+	
+	while (vornFrei()){
+		vor();
+	}
+	linksUm();
+	linksUm();
+	korn();
+}
+}
